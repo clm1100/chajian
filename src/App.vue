175cb730e373c10cdad847a0ca4cad6033f4afc4
clm110 <275298711@qq.com>
2018-03-14 23:33:38 +0800

    演示生命周期函数

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,41 @@
 <template>
+<div>
   <h1>{{$t("message.hello")}}</h1>
+  <input type="text" v-model="msg">
+  </div>  
 </template>
 <script>
 export default {
-  created () {
-    console.log(this.$t("message.hello"))
+  template:'',
+  data() {
+    return {
+      msg: "2222",
+      url: "../static/33.jpg",
+      logo: "./assets/logo.png"
+    };
+  },
+  render(){
+    console.log('999999999999999')
+  },
+  created() {
+    console.log(this.$t("message.hello"));
+  },
+  beforeMount () {
+    console.log("挂载钱")
+  },
+  mounted () {
+    console.log('mounted瓜子啊')
+  },
+  beforeUpdate() {
+    console.log("之前beforeUpdate");
+  },
+  updated () {
+    console.log("创建update")
   }
-}
+};
 </script>
 <style>
+
 </style>
 
 
